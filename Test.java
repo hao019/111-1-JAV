baabacad91e695094fcb66826abb69b9dac568b6
hao019 <113650572+hao019@users.noreply.github.com>
2022-09-23 11:41:45 +0800

    Add files via upload
    2

--- Test.java
+++ Test.java
@@ -2,7 +2,6 @@ public class Test{
     public static void main(String[] args){
         System.out.println(args[0]);
         System.out.println(args[1]);
-        return 0;
-        
-    }
+        System.out.println("Second Version");
+          }
 }
