a5e17a4395cd182e0605c244cc99f800c22fc36a
hao019 <113650572+hao019@users.noreply.github.com>
2022-09-30 10:45:40 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Test{
     public static void main(String[] args){
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println("Second Version");
-          }
+        int num1;
+        int num2;
+        int sum;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter.second interger:");
+        num1 = input.nextInt();
+        
+        System.out.println("Enter.second interger:");
+        num2 = input.nextInt();
+        
+        sum = num1 + num2;
+        System.out.println("The.sum is " + sum);
+    }
 }
