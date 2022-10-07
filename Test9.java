ccfd81fc72a90ca734e1b6dc7a57a414ac77610e
hao019 <113650572+hao019@users.noreply.github.com>
2022-10-07 10:53:18 +0800

    Add files via upload

---
+++ Test9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test9 {
+    public static void main(String[] args) {
+        int grade = 9 ;
+        
+        while (grade > -1) {
+            System.out.printf(" %d %n ", grade);
+            grade = grade - 1 ;
+        }
+    }
+}
