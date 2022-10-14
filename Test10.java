0e232b87c2a66c7e7e01ae6dfd814e9797811cec
hao019 <113650572+hao019@users.noreply.github.com>
2022-10-14 08:56:55 +0800

    Add files via upload

--- Test10.java
+++ Test10.java
@@ -4,7 +4,7 @@ public class Test10 {
     public static void main(String[] args) {
         int row = 0;
         
-        while (a < 5) {
+        while (row < 5) {
             int star = 0;
             while (star <= row) {
                 System.out.printf("*");
