00b5fbccbfc029b1e0a3b8d46d02951bbf0ecb00
hao019 <113650572+hao019@users.noreply.github.com>
2022-10-14 09:37:33 +0800

    Add files via upload

--- Test10.java
+++ Test10.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Test10 {
     public static void main(String[] args) {
         int row = 0;
+        int row2 = 0;
         
         while (row < 5) {
             int star = 0;
@@ -13,6 +14,17 @@ public class Test10 {
             System.out.println("");
             
             row = row + 1;
+        }
+        
+        while (row2 < 5) {
+            int star2 = 4;
+            while (star2 > row2) {
+                System.out.printf("*");
+                star2 = star2 - 1;
+            }
+            System.out.println("");
+            
+            row2 = row2 + 1;
         }
     }
 }
