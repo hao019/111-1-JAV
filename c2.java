0c4724243eb9e8d445069d92275394e20c8cd813
hao019 <113650572+hao019@users.noreply.github.com>
2022-11-04 10:01:21 +0800

    Add files via upload

---
+++ c2.java
@@ -0,0 +1,12 @@
+public class c2 {
+    public static void main(String[] args) {
+        int a;
+
+        a = add(7, 4);
+        System.out.printf("%d",a);
+    }
+
+    public static int add (int x, int y) {
+        return x + y;
+    }
+}
