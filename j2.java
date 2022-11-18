55a003b053841d092a14055e5bfdc8213ed6748a
hao019 <113650572+hao019@users.noreply.github.com>
2022-11-18 14:48:08 +0800

    Add files via upload

--- j2.java
+++ j2.java
@@ -5,11 +5,11 @@ public class j2	 {       //11月18加分題
         int a = 6;       //左極限
         int b = 6;       //右極限
 
-        for (int row = 1; row < 10 ;) {
+        for (int row = 1; row < 10 ; row++) {
 
             for (int col = 1; col <= 11; col++) {
 
-                if (col >= a && col <= b) {
+                if (col >= a && col <= b) {       //字元輸出選擇
                     System.out.print(" ");
                 }
                 
@@ -19,20 +19,18 @@ public class j2	 {       //11月18加分題
 
             }
             
-            if(row < 5) {
+            if(row < 5) {                         //橫、縱向輸出控制
                 star -= 1;
                 blank += 2;
-                a = a - 1;
-                b = b + 1;
-                row += 1;
+                a -= 1;
+                b += 1;
             }
 
             else{
                 star += 1;
                 blank -= 2;
-                a = a + 1;
-                b = b - 1;
-                row += 1;                
+                a += 1;
+                b -= 1;               
             }
 
             System.out.print("\n");
